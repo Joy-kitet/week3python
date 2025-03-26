48b841db45a5a0c89ec2b57e98f8da6bc8442533
Joy-kitet <debrajoykitet@gmail.com>
2025-03-26 20:59:18 +0300

    Control flows

---
+++ index.py
@@ -0,0 +1,14 @@
+grade =100
+
+if grade >=90:
+    print("A+")
+elif grade >=80:
+    print("A")
+elif grade >=70:
+    print("B+")
+elif grade >=60:
+    print("c")
+elif grade >= 50:
+    print("C")
+else :
+    print("E")
