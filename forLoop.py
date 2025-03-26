b591f04c01417fe62e06ad7155579d31748abc6f
Joy-kitet <debrajoykitet@gmail.com>
2025-03-26 21:56:19 +0300

    loops

---
+++ forLoop.py
@@ -0,0 +1,14 @@
+Countries =["Seychelles", "South Africa", "Rwanda", "UAE", "USA"]
+name = "Joy"
+
+#for country in Countries: 
+ #   print(f"I would love to visit {country}!")
+
+for country in Countries:
+      print(country)
+
+for Name in name:
+      print(Name)
+
+for i in range(5):
+    print(i)
